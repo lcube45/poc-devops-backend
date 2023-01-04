29b8a06a491410935a98b36144799d451370777c
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2023-01-04 11:59:23 +0100

    initial commit

---
+++ src/main/java/com/example/laurent/HelloWorldControler.java
@@ -0,0 +1,13 @@
+package com.example.laurent;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldControler {
+
+    @GetMapping("/")
+    public String bonjourLaurent() {
+        return "Hello World !";
+    }
+}
---
+++ src/test/java/com/example/laurent/LaurentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laurent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaurentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
