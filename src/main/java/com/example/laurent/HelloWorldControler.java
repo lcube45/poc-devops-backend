3f8fcfddc5a2d0840430189d61cd8667acf9e8f4
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2023-01-10 11:26:21 +0100

    new version 0.0.11

--- src/main/java/com/example/laurent/HelloWorldControler.java
+++ src/main/java/com/example/laurent/HelloWorldControler.java
@@ -8,6 +8,6 @@ public class HelloWorldControler {
 
     @GetMapping("/")
     public String bonjourLaurent() {
-        return "Hello World ! - Release 0.0.10";
+        return "Hello World ! - Release 0.0.11";
     }
 }
